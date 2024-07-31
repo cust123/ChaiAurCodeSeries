2c159b998db51531104c4b4a26db807fe9203d6c
Muhammad Shafiq <shafiqpesh123@gmail.com>
2024-08-01 02:30:58 +0500

    minor checking

--- Basics/newText.js
+++ Basics/newText.js
@@ -1,1 +1,9 @@
-console.log("setting up the repo and settings!");
+const myArray = 68676857;
+let userName = "Anni Programmer is German!";
+var oldUserName = "jena programmer and testor";
+
+console.log(myArray);
+console.log(userName);
+console.log(oldUserName);
+
+console.log(myArray, userName, oldUserName);
