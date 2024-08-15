55b4a7275f5cb7ff4f473a2ee0cb496a7985f6f1
Muhammad Shafiq <shafiqpesh123@gmail.com>
2024-08-15 12:17:40 +0500

    further changes

--- Basics/newText.js
+++ Basics/newText.js
@@ -2,8 +2,28 @@ const myArray = 68676857;
 let userName = "Anni Programmer is German!";
 var oldUserName = "jena programmer and testor";
 
-console.log(myArray);
-console.log(userName);
-console.log(oldUserName);
+// console.log(myArray);
+// console.log(userName);
+// console.log(oldUserName);
 
-console.log(myArray, userName, oldUserName);
+// console.log(myArray, userName, oldUserName);
+// Numbers and MATH
+// const score = 4000;
+// console.log(score);
+
+// const balance = new Number(1000);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// working with dates in JavaScript
+
+let myDate = new Date();
+
+console.log(myDate);
+
+console.log(myDate.toString());
+console.log(myDate.toLocaleString());
+
+console.log(typeof myDate);
