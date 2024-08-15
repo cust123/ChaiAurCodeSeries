d1354f92b84ee4f2dd8857185b2c8c1da7dd6508
Muhammad Shafiq <shafiqpesh123@gmail.com>
2024-08-15 20:07:47 +0500

    objects operations

--- Basics/objects.js
+++ Basics/objects.js
@@ -1,4 +1,21 @@
+const mySymobl = "myFirstSymbol";
+
 const myFirstObj = {
   name: "shafiq",
+  [mySymobl]: "myFirstSymbol",
+  age: 29,
+  education: "masters",
+  location: "Islamabad",
+  email: "shafiq@bxtechonlogies.net",
+  isLoggedIn: false,
 };
+console.log(myFirstObj);
+
+// console.log(myFirstObj.age);
+// console.log(myFirstObj["education"]);
+// console.log(myFirstObj[mySymobl]);
+
+// console.log(typeof myFirstObj.mySymobl);
+
+Object.freeze(myFirstObj);
 console.log(myFirstObj);
