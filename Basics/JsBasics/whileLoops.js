4be1a749cf5a0edcfc9928658e4cf060c7273f6d
Muhammad Shafiq <shafiqpesh123@gmail.com>
2024-08-24 16:35:33 +0500

    locations

---
+++ Basics/JsBasics/whileLoops.js
@@ -0,0 +1,20 @@
+// while loops
+let index = 0;
+while (index <= 10) {
+  //   console.log(`the value of index is : ${index}`);
+  index = index + 2;
+}
+
+let myArray = ["Flash", "Newton", "Smithy", "Johnthen"];
+let arr = 0;
+while (arr < myArray.length) {
+  //   console.log(`The Value of my My Array is : ${myArray[arr]}`);
+
+  arr++;
+}
+
+let score = 0;
+do {
+  console.log(`Current Score is :${score}`);
+  score++;
+} while (score <= 10);
