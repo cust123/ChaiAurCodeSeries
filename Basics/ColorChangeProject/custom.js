fc09ee0baf41d906cc10dc173f57899590b844a3
Muhammad Shafiq <shafiqpesh123@gmail.com>
2024-08-29 11:48:34 +0500

    project completed

--- Basics/ColorChangeProject/custom.js
+++ Basics/ColorChangeProject/custom.js
@@ -1,1 +1,36 @@
+// first we are getting the random color codes
 
+const randomColor = function () {
+  const hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+let intervalId;
+const myOriginalBgColor = "#212121";
+const startChangingColor = function () {
+  if (!intervalId) {
+    intervalId = setInterval(changeBgColor, 1000);
+  }
+
+  function changeBgColor() {
+    document.body.style.backgroundColor = randomColor();
+  }
+};
+const stopChangingColor = function () {
+  clearInterval(intervalId);
+  // intervalId = myOriginalBgColor;
+  intervalId = null;
+
+  // if the user wants to display the orginal bg color after clicking,
+  // then this pieace of code will work
+  if (intervalId === null) {
+    document.body.style.backgroundColor = myOriginalBgColor;
+  }
+};
+
+document.querySelector("#start").addEventListener("click", startChangingColor);
+
+document.querySelector("#stop").addEventListener("click", stopChangingColor);
