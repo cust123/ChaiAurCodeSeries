32b97c011e36cde0f01c88eb219e40aca2ca1d99
Muhammad Shafiq <shafiqpesh123@gmail.com>
2024-08-29 13:28:44 +0500

    new changes done

--- Basics/KeyBoardEvents/custom.js
+++ Basics/KeyBoardEvents/custom.js
@@ -1,1 +1,19 @@
-console.log("welcome to the key board events!");
+const insert = document.getElementById("insert");
+
+window.addEventListener("keydown", (e) => {
+  insert.innerHTML = `
+<div class="color">
+<table>
+  <tr>
+    <th>Key</th>
+    <th>keyCode</th>
+    <th>Code</th>
+  </tr>
+  <tr>
+    <td>${e.key === " " ? "Space" : e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+</table>
+</div>`;
+});
